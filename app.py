36548e2c4f43aeaeda02148b507b13dadcfd9178
parthp-7span <parth.p@7span.com>
2022-03-03 16:51:39 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+import utils
+
+app = Flask(__name__)
+api = Api(app)
+
+class Index(Resource):
+    def get(self):
+        return jsonify(message="captcha resolver endpoint...")
+
+class Predict(Resource):
+    def get(self):
+        return jsonify(message="send a post request...")
+
+    def post(self):
+        json_data = request.get_json(force=True)
+        img_uri = json_data['uri']
+        result = predictor.predict(img_uri)
+        return jsonify(message="success", result=result)
+
+api.add_resource(Predict,'/predict')
+api.add_resource(Index,'/')
+
+if __name__ == '__main__':
+    predictor = utils.Predictor()
+    app.run()
